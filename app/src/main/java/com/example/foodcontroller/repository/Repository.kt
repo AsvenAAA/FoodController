f7a17b6dbef4a1e1aeee3efa201bc64cb8ee226c
Edward <edwardgreatmen@gmail.com>
2020-05-03 09:52:21 +1000

    Добавлен ViewModel

---
+++ app/src/main/java/com/example/foodcontroller/repository/Repository.kt
@@ -0,0 +1,27 @@
+package com.example.foodcontroller.repository
+
+import androidx.lifecycle.LiveData
+import com.example.foodcontroller.repository.local.ProductDao
+import com.example.foodcontroller.repository.local.ProductEntity
+
+class Repository(private val productDao: ProductDao) {
+
+    val allProducts : LiveData<List<ProductEntity>> = productDao.getAllProducts()
+
+    suspend fun update(productEntity: ProductEntity) {
+        productDao.updateTarget(productEntity)
+    }
+
+    suspend fun insert(productEntity: ProductEntity) {
+        productDao.insert(productEntity)
+    }
+
+    suspend fun delete(productEntity: ProductEntity) {
+        productDao.deleteTarget(productEntity)
+    }
+
+    suspend fun deleteAll(productEntity: ProductEntity) {
+        productDao.deleteAllProducts()
+    }
+
+}
--- app/src/main/java/com/example/foodcontroller/viewmodel/FoodViewModel.kt
+++ app/src/main/java/com/example/foodcontroller/viewmodel/FoodViewModel.kt
@@ -3,14 +3,23 @@ package com.example.foodcontroller.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import com.example.foodcontroller.repository.local.TempData
+import com.example.foodcontroller.repository.Repository
+import com.example.foodcontroller.repository.local.ProductDao
+import com.example.foodcontroller.repository.local.ProductDatabase
+import com.example.foodcontroller.repository.local.ProductEntity
 
 class FoodViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository: TempData
-    val currentEatenFood: LiveData<Array<String>>
+
+    private val repository: Repository
+    val allProducts: LiveData<List<ProductEntity>>
 
     init {
-        repository = TempData()
-        currentEatenFood = repository.myData
+        val productDao = ProductDatabase.getDatabase(application).getProductDao()
+        repository = Repository(productDao)
+        allProducts = repository.allProducts
+    }
+
+    fun insert(productEntity: ProductEntity) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(productEntity)
     }
 }
